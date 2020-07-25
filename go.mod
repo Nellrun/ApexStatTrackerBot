51ae447100226f4b5589bc0455639b84773ddd18
shchesnyak <shchesnyak@yandex-team.ru>
2020-07-25 11:56:51 +0300

    web hook

--- go.mod
+++ go.mod
@@ -7,5 +7,9 @@ require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
 	github.com/lib/pq v1.7.0
+	github.com/manucorporat/sse v0.0.0-20160126180136-ee05b128a739 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	gopkg.in/bluesuncorp/validator.v5 v5.10.3 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,109 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"reflect"
 	"strings"
 
+	"github.com/gin-gonic/gin"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func messagesHandler() {
+var (
+	bot *tgbotapi.BotAPI
+)
+
+func webhookHandler(c *gin.Context) {
+	defer c.Request.Body.Close()
+
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var update tgbotapi.Update
+	err = json.Unmarshal(bytes, &update)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if update.Message == nil {
+		return
+	}
+
+	if update.Message.Photo != nil {
+		imageURL := (*update.Message.Photo)[0].FileID
+		command := ParseCommand(update.Message.Caption)
+		if command.name == "uploadimage" {
+			if len(command.args) < 1 {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "you must provide image tag")
+				bot.Send(msg)
+				return
+			}
+			err := AddImage(strings.ToLower(command.args[0]), imageURL)
+			if err != nil {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "something went wrong")
+				bot.Send(msg)
+				return
+			}
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("image for tag %s updated", command.args[0]))
+			bot.Send(msg)
+			return
+		}
+	}
+
+	if !strings.HasPrefix(update.Message.Text, "/") {
+		return
+	}
+
+	//Check if message from user is text
+	if reflect.TypeOf(update.Message.Text).Kind() == reflect.String && update.Message.Text != "" {
+		command := ParseCommand(update.Message.Text)
+		switch command.name {
+		case "chat_id":
+			ChatIDHandler(bot, update.Message.Chat.ID, command)
+		case "rank":
+			RankHandler(bot, update.Message.Chat.ID, command)
+		case "stats":
+			StatsHandler(bot, update.Message.Chat.ID, command)
+		case "subscribe":
+			SubscribeHandler(bot, update.Message.Chat.ID, command)
+		case "unsubscribe":
+			UnsubscribeHandler(bot, update.Message.Chat.ID, command)
+		case "deleteimage":
+			DeleteImageHandler(bot, update.Message.Chat.ID, command)
+		case "help":
+			HelpHandler(bot, update.Message.Chat.ID, command)
+		default:
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Меня писал очень плохой программист, и он не рассказал мне что значит эта комманда")
+			bot.Send(msg)
+		}
+	} else {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I don't understand you")
+		bot.Send(msg)
+	}
+}
+
+func main() {
+	err := CreateTables()
+	if err != nil {
+		panic(err)
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	//Create bot
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	bot, err = tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
 	if err != nil {
 		panic(err)
 	}
@@ -25,80 +116,16 @@ func messagesHandler() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	info, err := bot.GetWebhookInfo()
+
+	// gin router
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.POST("/"+bot.Token, webhookHandler)
+
+	err = router.Run(":" + port)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-	if info.LastErrorDate != 0 {
-		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
-	}
-	updates := bot.ListenForWebhook("/" + bot.Token)
-	// go http.ListenAndServeTLS("0.0.0.0", "cert.pem", "key.pem", nil)
 
-	for update := range updates {
-
-		if update.Message == nil {
-			continue
-		}
-
-		if update.Message.Photo != nil {
-			imageURL := (*update.Message.Photo)[0].FileID
-			command := ParseCommand(update.Message.Caption)
-			if command.name == "uploadimage" {
-				if len(command.args) < 1 {
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "you must provide image tag")
-					bot.Send(msg)
-					continue
-				}
-				err := AddImage(strings.ToLower(command.args[0]), imageURL)
-				if err != nil {
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "something went wrong")
-					bot.Send(msg)
-					continue
-				}
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("image for tag %s updated", command.args[0]))
-				bot.Send(msg)
-				continue
-			}
-		}
-
-		if !strings.HasPrefix(update.Message.Text, "/") {
-			continue
-		}
-
-		//Check if message from user is text
-		if reflect.TypeOf(update.Message.Text).Kind() == reflect.String && update.Message.Text != "" {
-			command := ParseCommand(update.Message.Text)
-			switch command.name {
-			case "chat_id":
-				ChatIDHandler(bot, update.Message.Chat.ID, command)
-			case "rank":
-				RankHandler(bot, update.Message.Chat.ID, command)
-			case "stats":
-				StatsHandler(bot, update.Message.Chat.ID, command)
-			case "subscribe":
-				SubscribeHandler(bot, update.Message.Chat.ID, command)
-			case "unsubscribe":
-				UnsubscribeHandler(bot, update.Message.Chat.ID, command)
-			case "deleteimage":
-				DeleteImageHandler(bot, update.Message.Chat.ID, command)
-			case "help":
-				HelpHandler(bot, update.Message.Chat.ID, command)
-			default:
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Меня писал очень плохой программист, и он не рассказал мне что значит эта комманда")
-				bot.Send(msg)
-			}
-		} else {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I don't understand you")
-			bot.Send(msg)
-		}
-	}
-}
-
-func main() {
-	err := CreateTables()
-	if err != nil {
-		panic(err)
-	}
-	messagesHandler()
 }
